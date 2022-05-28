725da3848e89fc2d0648f812a96c99588fedf37e
Peng Wei <mrpwei@outlook.com>
2022-05-28 17:23:56 +0800

    feat: 主页样式

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { Center } from "../Center";
+import { Container } from "./style";
+
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <Container>
+      {" "}
+      <Center>
+        {" "}
+        <a href="https://newline.co">Newline.co</a> {currentYear}{" "}
+      </Center>{" "}
+    </Container>
+  );
+};
