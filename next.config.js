86bbfe4a5dedf3a720eace73d6fbb314b742ecdc
Peng Wei <mrpwei@outlook.com>
2022-06-10 19:08:11 +0800

    remove next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-      imagesPublicPath: "/_next/static/images/",
-    },
-  ],
-]);
+module.exports = withPlugins([]);
